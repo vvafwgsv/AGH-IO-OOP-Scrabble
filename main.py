2c293dc82dc90cc63ed0821ba0e48e217366c5b3
vvafwgsv <mzieba>
2022-01-06 16:57:47 +0100

    main

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from PyQt6 import QtWidgets
 import sys
 import menu
+from gui_classes.login_window_controller import LoginWindowController
 
 if __name__ == '__main__':
 
+    import sys
     app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = menu.Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
+    ent = LoginWindowController()
     sys.exit(app.exec())
